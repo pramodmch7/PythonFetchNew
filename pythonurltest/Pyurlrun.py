9bd2aaec427bd3c70eee0b04390c0a68e332c167
pramodmch7 <pramodmc.h7@gmail.com>
2020-06-28 15:01:06 +0530

    Update Pyurlrun.py

--- pythonurltest/Pyurlrun.py
+++ pythonurltest/Pyurlrun.py
@@ -1,1 +1,2 @@
-print('HanUmaN form New 79 DVP 79!!!!!!!!97!!!!!!!777777799999997777777')
+# print('HanUmaN form New 79 DVP 79!!!!!!!!97!!!!!!!777777799999997777777')
+print('Ars Application Started Running in Port 67)
